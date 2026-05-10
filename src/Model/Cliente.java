9dd3ce316a01a2d1868a38ba591506c454370d33
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-05-10 18:42:37 -0300

    Criando os construtores getters e setters das classes Endereco e Cliente

--- src/Model/Cliente.java
+++ src/Model/Cliente.java
@@ -1,4 +1,70 @@
 package Model;
 
 public class Cliente {
+    private int idCliente;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String cpf;
+    private Endereco endereco;
+
+    public Cliente(){};
+
+
+    public Cliente(
+            int idCliente,
+            String nome,
+            String telefone,
+            String email,
+            String cpf,
+            Endereco endereco
+    ){
+        this.idCliente = idCliente;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public int getidCliente(){
+        return idCliente;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco(){
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco){
+        this.endereco = endereco;
+    }
 }
+
+
--- src/Model/Endereco.java
+++ src/Model/Endereco.java
@@ -1,4 +1,77 @@
 package Model;
 
 public class Endereco {
+
+    private int idEndereco;
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+    public Endereco(){};
+    public Endereco(
+            int idEndereco,
+            String logradouro,
+            String numero,
+            String bairro,
+            String cidade,
+            String estado,
+            String cep
+    ){
+        this.idEndereco = idEndereco;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+    }
+
+    public int getIdEndereco(){
+        return idEndereco;
+    }
+
+    public String getLogradouro(){
+        return logradouro;
+    }
+    public void setLogradouro(String logradouro){
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero(){
+        return numero;
+    }
+    public void setNumero(String numero){
+        this.numero = numero;
+    }
+
+    public String getBairro(){
+        return bairro;
+    }
+    public void setBairro(String bairro){
+        this.bairro = bairro;
+    }
+
+    public String getCidade(){
+        return cidade;
+    }
+    public void setCidade(String cidade){
+        this.cidade = cidade;
+    }
+
+    public String getEstado(){
+        return estado;
+    }
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+
+    public String getCep(){
+        return cep;
+    }
+    public void setCep(String cep){
+        this.cep = cep;
+    }
 }
