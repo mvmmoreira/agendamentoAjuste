06dafa4be6fe5656eefec148c073596009d90f5c
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-06-01 22:30:00 -0300

    teste para verificar se o metodo de salvar procedimento esta funcionando

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 import Connection.ConnectionFactory;
+import Dao.ProcedimentoDao;
+import Model.Procedimento;
 
 import java.sql.Connection;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        Connection conn = ConnectionFactory.getConnection();
-        if (conn != null){
-            System.out.println("Conexão realizada com sucesso");
-        }
+        Procedimento procedimento = new Procedimento(
+                0,
+                "Pilates",
+                "Recuperação",
+                120.80,
+                60
+        );
+        ProcedimentoDao procedimentoDao = new ProcedimentoDao();
+        procedimentoDao.salvar(procedimento);
+        System.out.println(procedimento);
     }
 }
